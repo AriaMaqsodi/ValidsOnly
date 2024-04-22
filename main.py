4e18a9901d38ef7e8eeb980e026ae29f121640d9
Aria Maqsodinezhad <83988129+AriaMaqsodi@users.noreply.github.com>
2024-04-22 03:50:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -73,31 +73,35 @@ plist = httpx.get(
 ).text
 plist = base64.b64decode(plist)
 
+results = dict()
+for item in plist.splitlines():
+    decoded_url = XrayUrlDecoder(item)
+    expose_local = xray_config_template % (decoded_url.generate_json_str())
+
+    thread = threading.Thread(target=run_subprocess, args=(expose_local,))
+    thread.start()
+
+    if not thread.is_alive():
+        print("Thread is not alive!")
+        continue
+
+    try:
+        data = httpx.get(
+            "https://open.spotify.com/", proxy="http://127.0.0.1:10509"
+        )
+        if data.status_code == 200:
+            results[decoded_url.name] = {"link": decoded_url.link, "response_time": data.elapsed.total_seconds()}
+            print(f"Working: {item}")
+
+    except Exception as e:
+        print("Error:")
+        print(e)
+        continue
+
+    finally:
+        if process.poll() is None:
+            os.kill(process.pid, signal.SIGTERM)
+
 with open("results.txt", "w", encoding="utf-8") as f:
-    for item in plist.splitlines():
-        decoded_url = XrayUrlDecoder(item.decode())
-        expose_local = xray_config_template % (decoded_url.generate_json_str())
-
-        thread = threading.Thread(target=run_subprocess, args=(expose_local,))
-        thread.start()
-
-        if not thread.is_alive():
-            print("Thread is not alive!")
-            continue
-
-        try:
-            data = httpx.get(
-                "https://open.spotify.com/", proxy="http://127.0.0.1:10509"
-            )
-            if data.status_code == 200:
-                f.write(f"{item.decode()}\n")
-                print(f"Working: {item.decode()}")
-
-        except Exception as e:
-            print("Error:")
-            print(e)
-            continue
-
-        finally:
-            if process.poll() is None:
-                os.kill(process.pid, signal.SIGTERM)
+    for _, value in sorted(results.items(), key=lambda item: item[1]["response_time"]):
+        f.write(f"{value['link']}\n")
